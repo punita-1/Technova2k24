9a8598406d683cb0df0eaaaaf8a766cb12995401
abhinavaa01 <agarwalabhinav91@gmail.com>
2024-03-08 13:47:42 +0530

    minor changes

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,6 +5,7 @@ import Passwordtoggle from "./misc/Passwordtoggle";
 import { useToaster, Message, Modal, Button, Placeholder } from "rsuite";
 import "rsuite/dist/rsuite.min.css";
 const SignUp = () => {
+  const [signin, setSignin] = useState(true);
 
   const aa = (e) => {
     e.preventDefault();
@@ -20,7 +21,13 @@ const SignUp = () => {
     <>
       <div className="bodybill d-flex justify-content-center align-items-center">
         <div className="mx-auto col-md-4 col-10 p-4 bg-light rounded-4 shadow border border-1 border-primary bg-opacity-50">
-          <div className="text-center fs-1 text-primary mb-4">SIGNUP</div>
+          <div className="text-center fs-1 text-primary mb-4">
+            <div className="btn btn-outline-primary d-flex justify-content-center align-items-center" onClick={()=>setSignin(()=>true)}>
+              <input type="radio" className="pt-5" checked={signin} />
+              <div className="fs-2">SIGNIN</div>
+            </div>
+            SIGNUP
+          </div>
           <form onSubmit={signup} className="row justify-content-center align-items-center" >
             <div className="input-group mb-3 col-10">
               <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill"></i></span>
